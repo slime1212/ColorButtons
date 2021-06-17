ec4335b80bcec1387fd4b7e4f2cd9ef63249f669
slime1212 <81129402+slime1212@users.noreply.github.com>
2021-06-16 20:38:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// Create 6 new color changing functions!
+
+function red() {
+    document.getElementById("color").style.backgroundColor = "#FF1C1C";
+};
+
+function orange() {
+    document.getElementById("color").style.backgroundColor = "#FF8000";
+};
+
+function yellow() {
+    document.getElementById("color").style.backgroundColor = "#FFEE00";
+};
+
+function green() {
+    document.getElementById("color").style.backgroundColor = "#1E9600";
+};
+
+function blue() {
+    document.getElementById("color").style.backgroundColor = "#006796";
+};
+
+function purple() {
+    document.getElementById("color").style.backgroundColor = "#820096";
+};
+
+// Last function: Make it back to default!
+
+function defaulted() {
+    document.getElementById("color").style.backgroundColor = "#FFF";
+};
+
+// Done!
